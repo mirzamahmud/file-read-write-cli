300281c160f04a35a3937817381aaac842085ac9
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-03-12 01:00:32 +0600

    add_test_file

--- test/file_read_write_cli_test.dart
+++ test/file_read_write_cli_test.dart
@@ -1,8 +1,7 @@
-import 'package:file_read_write_cli/file_read_write_cli.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('runCommand', () {
+    
   });
 }
