392506045a4944afe8eb70a22b48e73691ed26ad
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-03-12 01:00:11 +0600

    implement_command_runner

--- lib/file_read_write_cli.dart
+++ lib/file_read_write_cli.dart
@@ -1,3 +1,33 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+
+class CommandRunner{
+  static void runCommand(List<String> args){
+    // ====================== check if the user provided the correct number of arguments ===========================
+    if(args.length != 2){
+      print('Usage: dart run file_read_write_cli <input_file> <output_file>');
+      exit(1);
+    }
+
+    // ====================== get input and output file path from the arguments ====================================
+    final inputFile = args[0];
+    final outputFile = args[1];
+
+    try{
+      // ============================================== read input file ============================================
+      final input = File(inputFile);
+      final lines = input.readAsLinesSync();
+
+      // ================================= process each lines and convert to upper case ============================
+      final processedLines = lines.map((line) => line.toUpperCase()).toList();
+
+      // ================================= write the processed lines to the output file ============================
+      final output = File(outputFile);
+      output.writeAsStringSync(processedLines.join('\n'));
+
+      print('File processed successfully. Output written to $outputFile');
+    }catch(e){
+      // ============================================== handle error ===============================================
+      print('Error: $e');
+    }
+  }
 }
