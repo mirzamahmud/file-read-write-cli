486c4b8d2949bb2e4c9a117171db9df24e972b24
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-03-12 00:28:25 +0600

    init_project

---
+++ bin/file_read_write_cli.dart
@@ -0,0 +1,5 @@
+import 'package:file_read_write_cli/file_read_write_cli.dart' as file_read_write_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${file_read_write_cli.calculate()}!');
+}
---
+++ test/file_read_write_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:file_read_write_cli/file_read_write_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
